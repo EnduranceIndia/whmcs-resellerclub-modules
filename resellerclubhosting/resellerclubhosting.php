43fc7e9b20cc2e10171ffa572a474c53fa19328e
Viral Patel <viralpatel.info@gmail.com>
2012-02-28 19:33:48 +0530

    Fetched MDH plan names

--- resellerclubhosting/resellerclubhosting.php
+++ resellerclubhosting/resellerclubhosting.php
@@ -1,13 +1,22 @@
 <?php
 
 require dirname(__FILE__).DIRECTORY_SEPARATOR.'creds.php';
+require dirname(__FILE__).DIRECTORY_SEPARATOR.'orderbox.php';
 
 function resellerclubhosting_ConfigOptions() {
 
-	# Should return an array of the module options for each product - maximum of 24
-
+    $orderbox = orderbox_api_client( RESELLERCLUB_RESELLER_ID , RESELLERCLUB_RESELLER_PASSWORD );
+    $plans = $orderbox( 'GET', '/products/plan-details.json' );
+    
+    $mdh_plans = $plans['multidomainhosting'];
+    $mdh_plan_names = array();
+    
+    foreach ($mdh_plans as $plan) {
+        $mdh_plan_names[] = $plan['plan_name'];
+    }
+    
     $configarray = array(
-     "Hosting Plan" => array( "Type" => "dropdown", "Options" => "Dummy Plan 1, Dummy Plan 2", "Description" => "Select a hosting plan to associate with this product"),
+     "Hosting Plan" => array( "Type" => "dropdown", "Options" => implode(',', $mdh_plan_names) , "Description" => "Select a hosting plan to associate with this product"),
 	);
 
 	return $configarray;
